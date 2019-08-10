32ea3a963c4197a45f671ac41079181aaf58fae8
kjdeveloper <kjdeveloper247@gmail.com>
2019-08-10 11:14:54 +0200

    v01 - clean version of app

---
+++ src/main/java/pl/jankowiak/testform/TestformApplication.java
@@ -0,0 +1,13 @@
+package pl.jankowiak.testform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestformApplication.class, args);
+    }
+
+}
